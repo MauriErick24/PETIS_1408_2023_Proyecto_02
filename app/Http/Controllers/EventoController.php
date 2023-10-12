a05cc98533a58e8980c90ff513ad9979091b9d08
JuanDazaS <jadaza7832@gmail.com>
2023-10-11 22:58:20 -0400

    añadido validacion simple de campos

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Evento;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EventoController extends Controller
 {
@@ -25,28 +26,63 @@ class EventoController extends Controller
      */
     public function store(Request $request)
     {
-        $evento = new Evento();
-        $evento->nombre_evento = $request->input('nombre_evento');
-        $evento->inicio_inscripcion = $request->input('inicio_inscripcion');
-        $evento->fin_inscripcion = $request->input('fin_inscripcion');
-        $evento->fin_evento = $request->input('fin_evento');
-        $evento->organizador = $request->input('organizador');
-        $evento->imagen = $request->input('imagen');
-        $evento->lugar = $request->input('lugar');
-        $evento->email = $request->input('email');
-        $evento->descripcion = $request->input('descripcion');
-        $evento->hora = $request->input('hora');
-        $evento->telefono = $request->input('telefono');
-        $evento->requisito = $request->input('requisito');
-        $evento->premio = $request->input('premio');
-        $evento->reglas = $request->input('reglas');
-        $evento->detalle = $request->input('detalle');
-        $evento->afiche = $request->input('afiche');
-        $evento->contenido = $request->input('contenido');
-        $evento->invitado = $request->input('invitado');
-        $evento->tipoEvento_id = $request->input('tipoEvento_id');
-        $evento->save();
-        return response()->json('Registrado exitosamente', 201);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'nombre_evento' => 'required',
+                'inicio_inscripcion' => 'required|date',
+                'fin_inscripcion' => 'required|date',
+                'fin_evento' => 'required|date',
+                'organizador' => 'required',
+                'imagen' => 'required',
+                'lugar' => 'required',
+                'email' => 'required',
+                'descripcion' => 'required',
+                'hora' => 'required',
+                'telefono' => 'required|numeric',
+                'requisito' => 'required',
+                'premio' => 'required',
+                'reglas' => 'required',
+                'detalle' => 'required',
+                'afiche' => 'required',
+                'contenido' => 'required',
+                'invitado' => 'required',
+                'tipoEvento_id' => 'required|numeric'
+            ],
+            $messages = [
+                'required' => 'El campo :attribute es requerido',
+                'date' => 'El campo :attribute debe ser una fecha',
+                'numeric' => 'El campo :attribute debe contener solo numeros'
+            ]
+        );
+
+        if (!$validator->fails()) {
+            $evento = new Evento();
+            $evento->nombre_evento = $request->input('nombre_evento');
+            $evento->inicio_inscripcion = $request->input('inicio_inscripcion');
+            $evento->fin_inscripcion = $request->input('fin_inscripcion');
+            $evento->fin_evento = $request->input('fin_evento');
+            $evento->organizador = $request->input('organizador');
+            $evento->imagen = $request->input('imagen');
+            $evento->lugar = $request->input('lugar');
+            $evento->email = $request->input('email');
+            $evento->descripcion = $request->input('descripcion');
+            $evento->hora = $request->input('hora');
+            $evento->telefono = $request->input('telefono');
+            $evento->requisito = $request->input('requisito');
+            $evento->premio = $request->input('premio');
+            $evento->reglas = $request->input('reglas');
+            $evento->detalle = $request->input('detalle');
+            $evento->afiche = $request->input('afiche');
+            $evento->contenido = $request->input('contenido');
+            $evento->invitado = $request->input('invitado');
+            $evento->tipoEvento_id = $request->input('tipoEvento_id');
+            $evento->save();
+            return response()->json('Registrado exitosamente', 201);
+        } else {
+            return response()->json($validator->errors(), 422);
+            //return response()->json('no se pudo registrar los datos', 422);
+        }
     }
 
     /**
--- database/seeders/TipoEventoSeeder.php
+++ database/seeders/TipoEventoSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TipoEventoSeeder extends Seeder
 {
@@ -13,6 +14,19 @@ class TipoEventoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('tipo_eventos')->insert([
+            [
+                'tipo_evento' => 'Competencia'
+            ],
+            [
+                'tipo_evento' => 'Taller'
+            ],
+            [
+                'tipo_evento' => 'Entrenamiento'
+            ],
+            [
+                'tipo_evento' => 'Otro'
+            ]
+        ]);
     }
 }
